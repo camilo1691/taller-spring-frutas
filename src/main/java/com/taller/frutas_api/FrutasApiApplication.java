9fd018a2c4fb4377821f1d99b2383c21e8c57039
camilo1691 <CARDILAG6@UCENTRAL.EDU.CO>
2026-02-24 19:11:01 -0500

    estructura: se sube estructura de spring boot para iniciar desarrollo

---
+++ src/main/java/com/taller/frutas_api/FrutasApiApplication.java
@@ -0,0 +1,13 @@
+package com.taller.frutas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrutasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrutasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taller/frutas_api/FrutasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.frutas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrutasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
